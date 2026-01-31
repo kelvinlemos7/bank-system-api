7be7f7d0e7ed7e59bd52f76e6c94f17ed4007255
Kelvin Kauan <kelvinkauan17@gmail.com>
2026-01-31 13:17:14 -0300

    Security: move database credentials to environment variables

--- src/database.py
+++ src/database.py
@@ -1,11 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://bank_user:bank123@localhost:3306/bank_system_db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL não definida no ambiente")
 
 engine = create_engine(DATABASE_URL)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
 
